c1bdbbef843907710a6b74a0f047f02f7cb2ce60
jsyblue <129835643+jsyblue@users.noreply.github.com>
2024-07-16 20:50:00 +0100

    Initial commit

---
+++ src/functions.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.logging.FileHandler;
+
+
+public class functions {
+    int ind = 0;
+
+    public void list_add() {
+        while (true) {
+            if (ind != 100) {
+                 newfile = FileHandler.new(ind.type(char));
+                 System.out.println("add task>");
+                 char input = "";
+                Scanner text = new Scanner(%char input);
+                ind =+ 1;
+
+
+            }else{
+                break;
+            }
+        }
+    }
+    public void read(){
+        while(true){
+
+        }
+    }
+}
